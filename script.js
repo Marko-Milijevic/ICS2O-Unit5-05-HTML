087d70cb4fc04afc3d5400acc1363cdd5f736d12
MarkoMilijevic <Marko.Milijevic@ocsbstudent.ca>
2021-03-18 16:59:53 +0000

    Fixed errors

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // This allows the user to chose any number
-let  number= 0
+let number = 0
+
 // This button allows users to find the area of a square from an integer of their choosing
-document.getElementById('button').addEventListener 
-('click', multiplication)
+document.getElementById('button').addEventListener('click', multiplication)
+
 // When users click this button, the integer will mutiply itself and an alert wil appear stating the answer
-function multiplication () {
+function multiplication() {
   number = document.getElementById('input').value
   number = parseInt(number)
   number = number * number
